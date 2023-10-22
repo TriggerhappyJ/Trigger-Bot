050c04e7baae5f6723e7c6939bb8e61e15b274c0
TriggerhappyJ <jacob30wheeler@gmail.com>
2023-10-22 15:44:24 +1300

    Refactors and moved epic games api stuff to new script

---
+++ epicgames.py
@@ -0,0 +1,68 @@
+﻿from datetime import datetime, timezone
+from epicstore_api import EpicGamesStoreAPI
+
+api = EpicGamesStoreAPI()
+
+def dateConversion(start_date_iso, end_date_iso):
+    start_date = datetime.fromisoformat(start_date_iso)
+    end_date = datetime.fromisoformat(end_date_iso)
+    start_date = start_date.replace(tzinfo=timezone.utc).astimezone(tz=None)
+    end_date = end_date.replace(tzinfo=timezone.utc).astimezone(tz=None)
+    return start_date, end_date
+
+def getFreeGames():
+    free_games = api.get_free_games()['data']['Catalog']['searchStore']['elements']
+    free_games = list(sorted(
+        filter(
+            lambda g: g.get('promotions'),
+            free_games
+        ),
+        key=lambda g: g['title']
+    ))
+    return free_games
+
+def getGameData(game):
+    game_title = game['title']
+    game_publisher = game['seller']['name']
+    game_url = f"https://store.epicgames.com/en-US/p/{game['catalogNs']['mappings'][0]['pageSlug']}"
+    game_thumbnail = None
+    game_description = game['description']
+    for image in game['keyImages']:
+        if image['type'] == 'OfferImageWide':
+            game_thumbnail = image['url']
+    game_price = game['price']['totalPrice']['fmtPrice']['originalPrice']
+    game_promotions = game['promotions']['promotionalOffers']
+    upcoming_promotions = game['promotions']['upcomingPromotionalOffers']
+    return game_title, game_publisher, game_url, game_thumbnail, game_description, game_price, game_promotions, upcoming_promotions
+
+def currentFreeGames():
+    free_games_list = {}
+
+    for game in getFreeGames():
+        gameData = getGameData(game)
+        
+        if gameData[6] and game['price']['totalPrice']['discountPrice'] == 0:
+            promotion_data = gameData[6][0]['promotionalOffers'][0]
+            start_date_iso, end_date_iso = (
+                promotion_data['startDate'][:-1], promotion_data['endDate'][:-1]
+            )
+            dates = dateConversion(start_date_iso, end_date_iso)
+            free_games_list[gameData[0]] = [gameData[0], gameData[2], gameData[3], gameData[4], dates[0], dates[1]]
+
+    return free_games_list
+
+def upcomingFreeGames():
+    free_games_list = {}
+
+    for game in getFreeGames():
+        gameData = getGameData(game)
+
+        if gameData[7]:
+            promotion_data = gameData[7][0]['promotionalOffers'][0]
+            start_date_iso, end_date_iso = (
+                promotion_data['startDate'][:-1], promotion_data['endDate'][:-1]
+            )
+            dates = dateConversion(start_date_iso, end_date_iso)
+            free_games_list[gameData[0]] = [gameData[0], gameData[2], gameData[3], gameData[4], dates[0], dates[1]]
+
+    return free_games_list
--- triggerbot.py
+++ triggerbot.py
@@ -1,23 +1,66 @@
 ﻿import discord
-from discord.ext import commands
 from credentials import token
+from epicgames import currentFreeGames, upcomingFreeGames
+from datetime import datetime
 
-intents = discord.Intents.default()
-intents.message_content = True
+bot = discord.Bot()
 
-client = discord.Client(intents=intents)
+freeGames = bot.create_group("freegames", "Commands related to the Epic Games Store")
 
-@client.event
+@bot.event
 async def on_ready():
-    print('Logged in as {client.user}')
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
     
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-    
+@freeGames.command(guild_ids=[741435438807646268, 369336391467008002], name="current", description="Shows the current free games on the Epic Games Store")
+async def currentGames(ctx):
+    free_games_list = currentFreeGames()
+    for game in free_games_list:
+        gameName = free_games_list[game][0]
+        gameUrl = free_games_list[game][1]
+        gameThumbnail = free_games_list[game][2]
+        gameDescription = free_games_list[game][3]
+        startDate = free_games_list[game][4]
+        endDate = free_games_list[game][5]
         
-client.run(token)
+        embed = discord.Embed(color=0x353336)
+        embed.type = "rich"
+        embed.title = gameName
+        embed.url = gameUrl
+        embed.description = gameDescription
+        embed.add_field(name="Valid Until", value=str(discord.utils.format_dt(endDate)), inline=True)
+        embed.set_image(url=gameThumbnail)
+        # Shows the current local time
+        embed.set_footer(text="Last Updated: " + str(datetime.now())[:-7] + " || Developed by Jacob 😎")
+        embed.set_thumbnail(url="https://cdn2.unrealengine.com/Unreal+Engine%2Feg-logo-filled-1255x1272-0eb9d144a0f981d1cbaaa1eb957de7a3207b31bb.png")
+        await ctx.send(embed=embed)
+    await ctx.respond("There are a total of " + str(len(free_games_list)) + " free games right now <a:duckSpin:892990312732053544>")
+
+
+@freeGames.command(guild_ids=[741435438807646268, 369336391467008002], name="upcoming", description="Shows the upcoming free games on the Epic Games Store")
+async def upcomingGames(ctx):
+    free_games_list = upcomingFreeGames()
+    for game in free_games_list:
+        gameName = free_games_list[game][0]
+        gameUrl = free_games_list[game][1]
+        gameThumbnail = free_games_list[game][2]
+        gameDescription = free_games_list[game][3]
+        startDate = free_games_list[game][4]
+        endDate = free_games_list[game][5]
+
+        embed = discord.Embed(color=0x353336)
+        embed.type = "rich"
+        embed.title = gameName
+        embed.url = gameUrl
+        embed.description = gameDescription
+        embed.add_field(name="Available By", value=str(discord.utils.format_dt(startDate)), inline=True)
+        embed.set_image(url=gameThumbnail)
+        # Shows the current local time
+        embed.set_footer(text="Last Updated: " + str(datetime.now())[:-7] + " || Developed by Jacob 😎")
+        embed.set_thumbnail(url="https://cdn2.unrealengine.com/Unreal+Engine%2Feg-logo-filled-1255x1272-0eb9d144a0f981d1cbaaa1eb957de7a3207b31bb.png")
+        await ctx.send(embed=embed)
+    await ctx.respond("There are a total of " + str(len(free_games_list)) + " upcoming free games <a:duckSpin:892990312732053544>")
+
+bot.run(token)
