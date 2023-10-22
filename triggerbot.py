1505b6315b53d8779df08093eb44405ee13e8946
TriggerhappyJ <jacob30wheeler@gmail.com>
2023-10-22 13:15:58 +1300

    Added basic stuff

---
+++ triggerbot.py
@@ -0,0 +1,23 @@
+﻿import discord
+from discord.ext import commands
+from credentials import token
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print('Logged in as {client.user}')
+    
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+    
+        
+client.run(token)
