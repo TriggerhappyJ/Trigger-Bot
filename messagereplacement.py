2e15bd8e8f5023376450cad4dee6b58ecc8a8670
TriggerhappyJ <jacob30wheeler@gmail.com>
2023-11-16 11:18:42 +1300

    Replaced reaction with button for message deletion

--- messagereplacement.py
+++ messagereplacement.py
@@ -6,35 +6,36 @@ with open('config.yml', 'r') as config_file:
     config = yaml.safe_load(config_file)
     replacement_timeout = config['replacement_timeout']
 
+class MyView(discord.ui.View):
+    @discord.ui.button(label="", style=discord.ButtonStyle.red, emoji="🗑")
+    async def button_callback(self, button, interaction):
+        await interaction.response.send_message("Message has been deleted!", ephemeral=True)
+        if interaction.message:
+            await interaction.message.delete()
 
 async def handle_message_replacement(message, modified_message, worker, webhook, bot):
     author = message.author
     channel = message.channel
-    reaction_emoji = "☠"
 
     print("Replacing message from: " + author.name + " in channel: " + channel.name)
     await message.delete()
     sent_message, webhook = await send_replacement_message(modified_message, author, channel, reaction_emoji, webhook)
 
-    def check(message_reaction, user):
-        return user == author and str(message_reaction.emoji) == reaction_emoji and message_reaction.message.id == sent_message.id
+    # Create an instance of MyView and add it to the sent message
+    view = MyView()
+    await sent_message.edit(view=view)
 
-    try:
-        reaction, _ = await bot.wait_for("reaction_add", timeout=replacement_timeout, check=check)
-        if reaction.emoji == reaction_emoji:
-            await sent_message.delete()
-    except asyncio.TimeoutError:
-        await sent_message.clear_reaction(reaction_emoji)
+    # Set a timeout for the button to disappear
+    await asyncio.sleep(replacement_timeout)
+    await sent_message.edit(view=None)
 
     worker.cancel()
-
 
 async def send_replacement_message(modified_message, author, channel, reaction_emoji, webhook):
     webhook_name = author.display_name
     avatar = author.guild_avatar.url if author.guild_avatar else author.avatar.url
     async with channel.typing():
         sent_message = await webhook.send(str(modified_message), username=webhook_name, avatar_url=avatar, wait=True)
-        await sent_message.add_reaction(reaction_emoji)
     return sent_message, webhook
 
 
--- triggerbot.py
+++ triggerbot.py
@@ -206,4 +206,4 @@ async def task_consumer():
         job_queue.task_done()
 
 
-bot.run(token)
+bot.run(canary_token)
